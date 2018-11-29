d00317388730a843e64eb857ec7f3ddf3db41255
fsharpasharp <ppooii@gmail.com>
2018-11-29 11:11:43 +0100

    More fields, one is duplicated. Fix this later

--- statistics/models/Course.js
+++ statistics/models/Course.js
@@ -10,6 +10,12 @@ const schema = new mongoose.Schema({
   totalPass: { type: Number, default: 0 },
   totalFail: { type: Number, default: 0 },
   averageGrade: { type: Number, default: 0 },
+  U: { type: Number, default: 0 },
+  G: { type: Number, default: 0 },
+  TG: { type: Number, default: 0 },
+  3: { type: Number, default: 0 },
+  4: { type: Number, default: 0 },
+  5: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Course', schema);
