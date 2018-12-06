0a3f1f2c7e7e3580e881e4eb97e8370fe9315b80
Johan Winther <johan.winther@outlook.com>
2018-12-06 22:29:28 +0100

    Check for existing results file

--- statistics/update.sh
+++ statistics/update.sh
@@ -2,6 +2,12 @@
 if wget "https://document.chalmers.se/download?docid=00000000-0000-0000-0000-00001C968DC6" -O temp.xlsx > /dev/null; then
 
 newSize=$(stat -c%s temp.xlsx)
+if [ -f "results.xlsx" ]
+then
+	oldSize=$(stat -c%s results.xlsx)
+else
+	oldSize=0
+fi
 oldSize=$(stat -c%s results.xlsx)
 if [[ "$newSize" -gt "$oldSize" ]]; then
     mv -f temp.xlsx results.xlsx
