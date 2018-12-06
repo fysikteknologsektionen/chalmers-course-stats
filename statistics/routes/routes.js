98ee53a7ca3ffcadcae2dc6456c7a3c7604154ee
fsharpasharp <ppooii@gmail.com>
2018-12-06 17:06:46 +0100

    Removed redis

--- statistics/routes/routes.js
+++ statistics/routes/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const courseController = require('../controller/courseController.js');
-const cache = require('express-redis-cache')();
+// const cache = require('express-redis-cache')();
 
 const router = express.Router();
 
-router.get('/courses/', cache.route(), courseController.courseList);
+router.get('/courses/', courseController.courseList);
 router.get('/courses/:courseCode/', courseController.courseDetail);
 router.get('/results/:courseCode/', courseController.resultList);
 module.exports = router;
