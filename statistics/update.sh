3c9cdf58899ccf53e11d1a4042b03e50a5a463e6
Johan Winther <johan.winther@outlook.com>
2018-12-07 17:10:27 +0100

    Runnable as cron job

--- statistics/update.sh
+++ statistics/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if wget "https://document.chalmers.se/download?docid=00000000-0000-0000-0000-00001C968DC6" -O temp.xlsx > /dev/null; then
+cd "$(dirname "$0")";
+if wget "https://document.chalmers.se/download?docid=00000000-0000-0000-0000-00001C968DC6" -O "temp.xlsx" > /dev/null; then
 
 newSize=$(stat -c%s temp.xlsx)
 if [ -f "results.xlsx" ]
@@ -12,5 +13,8 @@ if [[ "$newSize" -gt "$oldSize" ]]; then
     mv -f temp.xlsx results.xlsx
     mongo --eval "db.results.drop();"
     node addFields.js
+    rm "temp.xlsx"
+else
+    echo "Downloaded file is not larger."
 fi
 fi
