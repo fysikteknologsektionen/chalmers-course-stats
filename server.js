f1315a71b58f31f1bde606c216f3cda89facab2d
fsharpasharp <ppooii@gmail.com>
2018-12-11 22:41:58 +0100

    Restored old lines

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ mongoose.connection.on('connected', () => {
   console.log('Mongoose on');
 });
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', router);
 
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log('Ready to go');
