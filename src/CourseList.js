b30a806a15b19327f6bd3200318399a2a0efd540
fsharpasharp <ppooii@gmail.com>
2018-12-19 10:42:17 +0100

    Added slash

--- src/CourseList.js
+++ src/CourseList.js
@@ -29,7 +29,7 @@ class CourseList extends React.Component {
       tempState.match = matchee;
       tempState.page = page;
       tempState.count = data.metadata[0].count;
-      this.props.history.push('/stats', tempState);
+      this.props.history.push('/stats/', tempState);
     }
   }
 
