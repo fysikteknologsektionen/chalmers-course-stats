e8c69d72ef8846c2c9bef5a92aa668af94aebeaf
Vesterlund <13664409+Vesterlund@users.noreply.github.com>
2021-11-25 17:40:28 +0100

    Notice update

--- src/Notice.js
+++ src/Notice.js
@@ -5,22 +5,19 @@ class Notice extends React.Component {
 
   render() {
     return (
-      <div className={'notification is-warning'+( Notice.hidden ? ' is-hidden':'')}>
+      <div className={'notification is-success' + (Notice.hidden ? ' is-hidden' : '')}>
         <button className="delete" onClick={this.hideNotice}></button>
-        <p>Chalmers has stopped publishing the course results file which is needed for updates.
-        For more information, visit <a href="https://github.com/Fysikteknologsektionen/chalmers-course-stats/issues/39" rel="noopener noreferrer">this issue on GitHub</a>.
-        We are currently looking into other ways of obtaining this information.
-        Note, however, that you can still browse the old results.
-        Thank you for your patience.</p>
+        <p>We're back up and running! The latest exams should be available now.
+          However, there are still some results missing from 2020 and early 2021 which are being looked into if they can be obtained.
+        </p>
       </div>
-      );
-    }
-    
-    hideNotice = () => {
-      Notice.hidden = true;
-      this.forceUpdate();
-    }
+    );
   }
-  
-  export default Notice;
-  
+
+  hideNotice = () => {
+    Notice.hidden = true;
+    this.forceUpdate();
+  }
+}
+
+export default Notice;
