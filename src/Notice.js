9e164d80b8020e510ff5648b1277221cf7c9e722
Vesterlund <13664409+Vesterlund@users.noreply.github.com>
2021-11-25 18:17:59 +0100

    Notice update

--- src/Notice.js
+++ src/Notice.js
@@ -8,7 +8,7 @@ class Notice extends React.Component {
       <div className={'notification is-success' + (Notice.hidden ? ' is-hidden' : '')}>
         <button className="delete" onClick={this.hideNotice}></button>
         <p>We're back up and running! The latest exams should be available now.
-          However, there are still some results missing from 2020 and early 2021 which are being looked into if they can be obtained.
+          However, there are still some results missing from 2019, 2020 and early 2021 which are being looked into if they can be obtained.
         </p>
       </div>
     );
